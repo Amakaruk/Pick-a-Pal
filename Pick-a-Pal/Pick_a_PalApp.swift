064ed0b8ae54883c874a66d17d743d76ba02b46f
Aaron Makaruk <aaronmakaruk@gmail.com>
2026-01-21 14:36:02 -0700

    init

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Aaron Makaruk on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
